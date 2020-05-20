c32dc99d7521a7fe6a1cf5ee32660467331ad3ff
Kimia Darden <kimiadarden@gmail.com>
2020-05-20 13:45:57 -0400

    adding resume to the side bar

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,7 +4,13 @@ import React, { Component } from 'react';
 class About extends Component {
   render() {
     return(
-      <div><h1>Contact me</h1></div>
+            <div className="App" >
+
+        
+        
+        <h1>Contact me</h1>
+        
+        </div>
     )
   }
 }
--- src/components/main.js
+++ src/components/main.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Landing from './landing';
-import AboutMe from './aboutme';
 import Contact from './contact';
 import Projects from './projects/projects';
 
@@ -10,7 +9,6 @@ import Projects from './projects/projects';
 const Main = () => (
     <Switch>
       <Route exact path="/" component={Landing} />
-      <Route path="/aboutme" component={AboutMe} />
       <Route path="/contact" component={Contact} />
       <Route path="/projects" component={Projects} />
     </Switch>
