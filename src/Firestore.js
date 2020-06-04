5e69a48a89989db8218a252540108c8df360c355
Kimia Darden <kimiadarden@gmail.com>
2020-06-03 21:48:41 -0400

    .env file update

--- src/Firestore.js
+++ src/Firestore.js
@@ -2,12 +2,12 @@ import firebase from "firebase";
 
 
 var firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    databaseURL: process.env.DATABASE_URL,
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
     projectId: "kimiaportfolio-f9c8c",
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   };
 
 
